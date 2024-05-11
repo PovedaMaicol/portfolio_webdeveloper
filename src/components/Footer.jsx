fa3b2dca2f69629b64aaa00ac2b32a1ed1c02ae0
Maicol Salazar <stivsal12@gmail.com>
2024-05-11 15:54:31 -0500

    send emails with Email.js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,64 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './styles/footer.css';
 import { useTranslation } from "react-i18next";
+import emailjs from '@emailjs/browser';
 
 const Footer = () => {
+  // translate
   const [t, i18n] = useTranslation("global");
+  // formulario
+  const form = useRef();
+  // mostrar mensaje
+  const [showMessage, setIsShowMessage] = useState(false);
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_xzul74k', 'template_d8hakac', form.current, {
+        publicKey: 'd-nqlcfP9XcmllwWV',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+
+      setIsShowMessage(true);
+      setTimeout(() => {
+        setIsShowMessage(false);
+      }, 3000);
+  };
+
+
+
   return (
   <div className='footer_cont container'>
 
 
     <div className='info_container'>
- <section className='form'>      
+
+
+<form ref={form} onSubmit={sendEmail} className='form'> 
+
+<div className={`msjSend ${showMessage ? 'visible' : 'hidden'}`}><i className='bx bx-mail-send'></i><h2>{t("footer.alert")}</h2></div>
+
 <div className="content-form">
 <h3 className="txt-descriptivo">{t("footer.contactme")}</h3>
 <p className="contacts-txt">{t("footer.t")}</p>
-<input className="contacts-register" type="text" id="name" name="name" placeholder={t("footer.name")}></input>
-<input className="contacts-register" type="email" id="email" name="email" placeholder={t("footer.email")}></input>
-<textarea className="contacts-register" type="messaje" id="messaje" name="messaje" rows="4"
-placeholder={t("footer.message")}></textarea>
-<input className="button-send" type="submit" value={t("footer.send")}></input>
-</div>
-</section> 
+
+      <input className="contacts-register" type="text" name="user_name" placeholder={t("footer.name")}/>
+      
+      <input className="contacts-register" type="email" name="user_email" placeholder={t("footer.email")}/>
+     
+      <textarea className="contacts-register" name="message" placeholder={t("footer.message")}/>
+      <button className="button-send" type="submit" value="Send">{t("footer.send")}</button>  
+</div>  
+</form> 
 <br/>
 
 
